0cf816fde5682df2c2c302c498d3e2b9a7d70a5c
Carmen Arias <maricarmen98a@gmail.com>
2023-07-10 20:21:21 +0200

    Added input and api call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GooglemapsService } from './services/googlemaps.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,9 +12,14 @@ export class AppComponent implements OnInit {
   datas: any = [];
   showInfo: boolean = false;
   clearId: any;
-  constructor() {}
+  input: any;
+
+  constructor( public backendService: GooglemapsService) {}
+
   ngOnInit(): void {}
+
   trackMe() {
+    this.sendData()
     if (navigator.geolocation) {
       this.clearId = navigator.geolocation.watchPosition((position) => {
         this.showTrackingPosition(position);
@@ -25,6 +31,16 @@ export class AppComponent implements OnInit {
     } else {
       alert("Geolocation is not supported by this browser.");
     }
+  }
+  sendData() {
+    let object = {IdUID: this.input};
+    const formData = new FormData();
+    formData.append('IdUID', this.input );
+    this.backendService.login(formData).subscribe((data: any) => {
+      console.log(data)
+    }, (error: Error) => { 
+      console.log(error)
+    })
   }
   showTrackingPosition(position: GeolocationPosition) {
     this.showInfo = true;
@@ -50,5 +66,8 @@ export class AppComponent implements OnInit {
   stop() {
     navigator.geolocation.clearWatch(this.clearId);
     console.log('Stopped tracking successfully')
+  }
+  getValue(event: any) {
+    this.input = event.target.value;
   }
 }
--- src/app/services/googlemaps.service.ts
+++ src/app/services/googlemaps.service.ts
@@ -9,9 +9,13 @@ import { HttpClient } from '@angular/common/http';
 })
 export class GooglemapsService {
 
+  apiUrl = 'https://back.rail-travel-project.com/'; //https://34.147.140.133/
   constructor(public http: HttpClient) { }
 
   storeData(data: DataDTO): Observable<any> {
     return this.http.post(environment.apiUrl + 'data', data);
   }
+  login(data: any): Observable<any> {
+    return this.http.post( this.apiUrl + 'User/login', data);
+  } 
 }
